e021001b62bc71f992b6c8bf36d2ad706aa08791
Keratuwe Pheko <>
2021-05-20 13:37:14 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const paddle= document.getElementById("paddle");
+const ball = document.getElementById("ball");
+const xdir = true;
+
+const paddleMove = (event) => {
+  paddle.style.left = event.offsetX + "px";
+}
+
+const update = () =>{
+  ball.style.top = ball.offsetTop + 1 + 'px';
+  ball.style.left = ball.offsetLeft + 1 + 'px';
+}
+const tick = setInterval(update, 100);
